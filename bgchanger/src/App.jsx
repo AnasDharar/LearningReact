9480c8dde79d1bf1e4ece26fffd43edb02a61614
Anas <86509653+AnasDharar@users.noreply.github.com>
2025-11-08 17:44:56 +0530

    bgchanger

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -1,33 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [color,setColor] = useState('bg-gray-500');
+  function changecolor(x){
+    setColor(x);
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={`${color} justify-center items-center w-screen h-screen flex`}>
+        <div className='p-3 border-2 border-black rounded-md bg-gray-800 shadow-amber-50 shadow-md'>
+          <button className='p-4 bg-red-500 m-4 shadow-2xl rounded-2xl text-white' onClick={() => changecolor('bg-red-500')}>Red</button>
+          <button className='p-4 bg-green-500 m-4 shadow-2xl rounded-2xl text-white' onClick={() => changecolor('bg-green-500')}>Green</button>
+          <button className='p-4 bg-blue-500 m-4 shadow-2xl rounded-2xl text-white' onClick={() => changecolor('bg-blue-500')}>Blue</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
